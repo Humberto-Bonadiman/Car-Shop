41a035ce3031f167a90cfacb27a210925fdeabc1
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-04-11 15:16:00 -0400

    vehicleInterface

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+const VehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int() });
+
+export type Vehicle = z.infer<typeof VehicleSchema>;
