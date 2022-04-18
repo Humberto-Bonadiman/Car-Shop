4c31036c9302cf0c446106860df114269efb0bee
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-04-18 18:16:10 -0400

    fix: use correct database

--- src/connection.ts
+++ src/connection.ts
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 
 // dotenv.config();
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
