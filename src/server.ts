b98d1378c367fcc4e0a604f7473ed5f076554b37
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-04-18 17:45:33 -0400

    fix: add motorcycle to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import CarsController from './controllers/CarsController';
 import { Car } from './interfaces/CarInterface';
 import CustomRouter from './routes/Router';
 import App from './app';
+import MotorcycleController from './controllers/MotorcycleController';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
@@ -11,5 +13,12 @@ const carRouter = new CustomRouter<Car>();
 carRouter.addRoute(carsController);
 
 server.addRouter(carRouter.router);
+
+const motorcycleController = new MotorcycleController();
+
+const motorcycleRouter = new CustomRouter<Motorcycle>();
+motorcycleRouter.addRoute(motorcycleController);
+
+server.addRouter(motorcycleRouter.router);
 
 export default server;
